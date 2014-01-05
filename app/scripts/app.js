bdad3ee12c521444a33f9a2c4153848cadf5dec5
Travis Grathwell <tjgrathwell@gmail.com>
2014-01-04 20:07:51 -0800

    Simple 2-d board that allows selecting and deselecting cells

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,15 +1,10 @@
 'use strict';
 
-angular.module('ngPicrossApp', [
-  'ngRoute'
-])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
+angular.module('ngPicrossApp', ['ngRoute']).config(function ($routeProvider) {
+  $routeProvider.when('/', {
+    templateUrl: 'views/main.html',
+    controller: 'MainCtrl'
+  }).otherwise({
+    redirectTo: '/'
   });
+});
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,22 @@
 'use strict';
 
-angular.module('ngPicrossApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+angular.module('ngPicrossApp').controller('MainCtrl', function ($scope) {
+  var rows = 15;
+  var cols = 15;
+  $scope.board = [];
+  for (var i = 0; i < rows; i++) {
+    var row = [];
+    for (var j = 0; j < cols; j++) {
+      row.push('');
+    }
+    $scope.board.push(row);
+  }
+
+  $scope.clickedCell = function (rowIndex, colIndex) {
+    if ($scope.board[rowIndex][colIndex]) {
+      $scope.board[rowIndex][colIndex] = '';
+    } else {
+      $scope.board[rowIndex][colIndex] = 'x';
+    }
+  };
+});
