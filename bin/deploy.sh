6b269c9049d86f9b029bd043e7176281306ca989
Travis Grathwell <tjgrathwell@gmail.com>
2014-08-05 23:39:12 -0700

    Add deploy script to automate pushing to gh-pages

---
+++ bin/deploy.sh
@@ -0,0 +1,22 @@
+git branch -D gh-pages
+
+git checkout -b gh-pages
+
+perl -i -nle'/^dist$/||print' .gitignore
+
+grunt build
+
+git add .
+
+git commit -m "Adding dist"
+
+git filter-branch -f --env-filter "
+    GIT_AUTHOR_NAME='nobody'
+    GIT_AUTHOR_EMAIL='nobody-gh-pages@example.com'
+    GIT_COMMITTER_NAME='nobody'
+    GIT_COMMITTER_EMAIL='nobody-gh-pages@example.com'
+  " HEAD
+
+git push origin `git subtree split --prefix dist gh-pages`:gh-pages --force
+
+git checkout master
