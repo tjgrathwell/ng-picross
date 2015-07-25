cf8e01f9167556c36c4819f6510636f4ea0086b0
Travis Grathwell <tjgrathwell@gmail.com>
2015-07-24 23:24:01 -0700

    Solution benchmark table is sortable by puzzle/timing

--- app/scripts/controllers/puzzleSolverBenchmarkCtrl.js
+++ app/scripts/controllers/puzzleSolverBenchmarkCtrl.js
@@ -4,9 +4,20 @@ angular.module('ngPicrossApp').controller('PuzzleSolverBenchmarkCtrl', function 
   var allPuzzles = puzzleCatalogService.getAvailablePuzzles();
   $scope.solutionTimes = [];
 
+  $scope.sortColumn = 'id';
+  $scope.sortReverse = false;
+
   $scope.totalTime = 0;
 
   $scope.solving = true;
+
+  $scope.orderBy = function (newColumn) {
+    if (newColumn === $scope.sortColumn) {
+      $scope.sortReverse = !$scope.sortReverse;
+    } else {
+      $scope.sortColumn = newColumn;
+    }
+  };
 
   function benchmarkPuzzle () {
     var start = Date.now();
@@ -23,7 +34,7 @@ angular.module('ngPicrossApp').controller('PuzzleSolverBenchmarkCtrl', function 
 
       var timeTaken = (Date.now() - start) / 1000;
 
-      $scope.solutionTimes.push({id: listPuzzle.id, time: timeTaken});
+      $scope.solutionTimes.push({id: parseInt(listPuzzle.id, 10), time: timeTaken});
 
       $scope.totalTime += timeTaken;
 
